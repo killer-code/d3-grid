564fbfa263b09b8b8fc9338a353300de2ede7904
Dmitry <killer_code@bk.ru>
2021-06-17 21:20:35 +0400

    add bars && transorm && scale bars

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -15,60 +15,46 @@ export default {
   mounted() {
     const self = this;
     const today = new Date();
-    var mindate = new Date(2021,5,4),
-        maxdate = new Date(2021,5,18);
-    // 10,000 random data points
-    var data = this.$d3.range(1, 10000).map(function(d) {
-      return {
-        i: d,
-        x: mindate,
-        y: 5,
-      }
-    });
+    var mindate = this.$moment(today)._d,
+        maxdate = this.$moment(mindate).add('14', 'days');
 
     const tasks = [
       {
+        user_id: 1,
         user: 'user_1',
         isIncluded: true,
         text: 'name task 1',
         times: {
-          starting_time: [2021,5,15],
-          ending_time: [2021,5,20]
+          starting_time: [2021,6,14],
+          ending_time: [2021,6,20]
         }
       },
       {
+        user_id: 2,
         user: 'user_2',
         isIncluded: true,
         text: 'name task 2',
         times: {
-          starting_time: new Date(2021,5,15),
-          ending_time: new Date(2021,5,20)
+          starting_time: [2021,6,15],
+          ending_time: [2021,6,16]
         }
       },
       {
+        user_id: 2,
         user: 'user_2',
         isIncluded: true,
         text: 'name task 3',
         times: {
-          starting_time: new Date(2021,5,25),
-          ending_time: new Date(2021,6,20)
+          starting_time: [2021,6,21],
+          ending_time: [2021,6,23]
         }
       }
     ];
 
     const dataObj = [
-      {
-        id: 1,
-        user: 'user 1',
-        start: new Date(),
-        end: new Date(2021,5,10)
-      },
-      {
-        id: 2,
-        user: 'user 2',
-        start: new Date(),
-        end: new Date(2021,5,10)
-      }
+      { id: 1, user: 'user 1'},
+      { id: 2, user: 'user 2'},
+      { id: 3, user: 'user 3'}
     ];
 
     var svg = this.$d3.select("svg"),
@@ -82,21 +68,21 @@ export default {
       height = dataObj.length * 100,
       g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // large "endless" zoom
     var zoom = this.$d3.zoom()
-      .scaleExtent([1, 6])
-      .on("zoom", zoomed);
+      .scaleExtent([.5, 6])
+      .on("zoom", zoomed)
+        
 
     var x = this.$d3.scaleTime()
       .domain([mindate, maxdate])
       .range([250, width]);
 
     var y = this.$d3.scaleBand()
-      .domain(dataObj.map(d => d.user))
+      .domain(dataObj.map(d => d.user).reverse())
       .rangeRound([height, 0])
 
     var xAxis = this.$d3.axisBottom(x)
-      .ticks((width - 250) / 100)
+      .ticks(self.$d3.time)
       .tickPadding(10)
       .tickSize(-height);
 
@@ -104,7 +90,6 @@ export default {
       .ticks(dataObj.length)
       .tickSize(width)
       .tickPadding(-width)
-      
 
     var gX = svg.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -122,7 +107,9 @@ export default {
     var gY = svg.append("g")
       .attr("class", "axis axis--y")
       .style("font-size", "14px")
-      .call(yAxis);
+      .call(yAxis)
+      .selectAll('text')
+      .attr('fill', 'red')
 
     // BARS
     const bars = svg.selectAll(".bar")
@@ -132,64 +119,28 @@ export default {
     
     bars.append("rect")
         .attr("class", "bar")
-        .attr("x", d => 250 )
-        .attr("y", d => 100)
-        .attr("height", height / dataObj.length)
+        .attr("x", d => x(new Date(d.times.starting_time)))
+        .attr("y", d => {
+          const label = dataObj.find(obj => obj.id === d.user_id);
+          const index = dataObj.indexOf(label)
+          
+          return index * 100
+        })
+        .attr("height", (height / dataObj.length) - 2)
         .attr("width", d => {
-          let days = self.$moment.duration(self.$moment(tasks[0].times.ending_time).diff(tasks[0].times.starting_time, 'days'))
-          days = Number(days);
-          return days * (width - 250) / 14
-        });
+          return self.$d3.timeDay.count(new Date(d.times.starting_time), new Date(d.times.ending_time))  * (width - 250) / 14
+        })
+        .attr('fill', '#00ff00')
 
     svg.call(zoom);
-    console.log((window.innerWidth - 250) / 14)
 
     function zoomed() {
-      var t = self.$d3.event.transform,
-      sx = t.rescaleX(x);
+      var t = self.$d3.event.transform;
+      var sx = t.rescaleX(x);
 
       gX.call(xAxis.scale(sx));
-
-      // updateData(sx)
+      bars.style('transform', `translateX(${t.x}px) scaleX(${t.k})`);
     }
-
-    // classic enter, update, exit pattern
-    // function updateData(sx, sy) {
-
-    //   // filter are data to those points in range
-    //   var f = data.filter(function(d) {
-    //     return (
-    //       d.x > sx.domain()[0] &&
-    //       d.x < sx.domain()[1] &&
-    //       d.y > sy.domain()[0] &&
-    //       d.y < sy.domain()[1]
-    //     )
-    //   });
-
-    //   var s = g.selectAll(".point")
-    //     .data(f, function(d) {
-    //       return d.i;
-    //     });
-
-    //   // remove those out of range
-    //   s.exit().remove();
-
-    //   // add the new ones in range
-    //   s = s.enter()
-    //     .append('circle')
-    //     .attr('class', 'point')
-    //     .attr('r', 10)
-    //     .style('fill', 'steelblue')
-    //     .merge(s);
-
-    //   // update all in range
-    //   s.attr('cx', function(d) {
-    //       return sx(d.x);
-    //     })
-    //     .attr('cy', function(d) {
-    //       return sy(d.y);
-    //     });
-    // }
   }
 }
 </script>
